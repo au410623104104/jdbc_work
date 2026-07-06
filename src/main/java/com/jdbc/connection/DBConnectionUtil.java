c7eaed7bbc0f26546bef201f4ff7ef171c42934d
shaikshabreen <shaikshabreen14@gmail.com>
2026-07-06 11:56:51 +0530

    instilized by shabreen

---
+++ src/main/java/com/jdbc/connection/DBConnectionUtil.java
@@ -0,0 +1,21 @@
+package com.jdbc.connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+      public class DBConnectionUtil
+    {
+        private static final String DATABASE_DRIVER = "com.mysql.cj.jdbc.Driver";
+        private static final String DATABASE_URL = "jdbc:mysql://localhost:3306/student_jdbc";
+        private static final String DATABASE_USERNAME = "root";
+        private static final String DATABASE_PASSWORD = "Mysql@123";
+
+        public static Connection getConnection() throws SQLException
+        {
+            return DriverManager.getConnection(DATABASE_URL, DATABASE_USERNAME, DATABASE_PASSWORD);
+        }
+
+    }
+
+
